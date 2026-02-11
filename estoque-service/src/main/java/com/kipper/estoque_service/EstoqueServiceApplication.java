8ac01930ee20298b029d4e5832263817959b9ac9
Hiran Ferreti Baccos <hiran.baccos.prestserv@petrobras.com.br>
2026-02-11 20:09:39 -0300

    first commit

---
+++ estoque-service/src/main/java/com/kipper/estoque_service/EstoqueServiceApplication.java
@@ -0,0 +1,11 @@
+package com.kipper.estoque_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoqueServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EstoqueServiceApplication.class, args);
+    }
+}
